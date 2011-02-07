0daf15904e4199beb18d1cf906d0ac0136bfb812
Sam Rose <samuel.rose@gmail.com>
2011-02-07 13:22:01 -0500

    commit latest changes

--- Gemfile
+++ Gemfile
@@ -25,7 +25,7 @@ gem 'coderay'
 gem 'lesstile'
 gem 'formtastic'
 gem 'will_paginate', '= 3.0.pre2'
-
+gem 'disqus'
 # Bundle gems for the local environment. Make sure to
 # put test-only gems in this group so their generators
 # and rake tasks are available in development mode:
--- config/application.rb
+++ config/application.rb
@@ -5,7 +5,8 @@ require 'rails/all'
 # If you have a Gemfile, require the gems listed there, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(:default, Rails.env) if defined?(Bundler)
-
+Disqus::defaults[:account] = 'futureforwardinstituteblog'
+Disqus::defaults[:developer] = true # if you want to test it on the localhost
 module Enki
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
